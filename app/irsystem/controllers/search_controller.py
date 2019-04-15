7f0c41e0f9569aa6028d4ac306bab5d6fe803d1c
Bowen Mao <b.mao@columbia.edu>
2019-04-14 22:41:44 -0400

    Combined front-end with back-end
    Combined front-end with back-end.

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -6,6 +6,7 @@ project_name = "BookHub"
 net_id = "Minghao Li: ml922; Bowen Mao: bm644; Lauren Wong: lqw5; Lu Yang: ly298"
 
 DATA_DIR = os.path.abspath(os.path.join(__file__, "..", "..", "..", "data", "merged"))
+MAX_COMPARED = 20
 MAX_RECOMMEND = 8
 MAX_REVIEWS = 5
 
@@ -26,7 +27,7 @@ def _get_cos_sim_desc(book_ids: list) -> list:
 		if book_id in top_k_cos_sim:
 			for similar_book_id in top_k_cos_sim[book_id]:
 				cos_sim_sum[similar_book_id] += top_k_cos_sim[book_id][similar_book_id]
-	result = sorted(cos_sim_sum, key=cos_sim_sum.get, reverse=True)[:MAX_RECOMMEND]
+	result = sorted(cos_sim_sum, key=cos_sim_sum.get, reverse=True)[:MAX_COMPARED]
 	return result
 
 
@@ -39,27 +40,30 @@ def _get_book_reviews(book_id: str) -> list:
 		reviews = json.load(fin2)
 	for review_id in review_ids:
 		review_texts.append(reviews[str(review_id)]["body"])
-	return review_texts
+	return review_texts[:MAX_RECOMMEND]
 
 
-def _get_recommended_books_detail(recommended_book_ids: list) -> list:
+def _get_recommended_books_detail(recommended_book_ids: list, request_book_ids: list) -> list:
 	recommended_books = []
 	with open(os.path.join(DATA_DIR, "books.json"), "r") as fin:
 		books = json.load(fin)
+	titles = [books[book_id]["title"] for book_id in request_book_ids]
 	for book_id in recommended_book_ids:
-		book = {
-			"id": "id" + book_id,
-			"title": books[book_id]["title"],
-			"average_rating": float(books[book_id]["average_rating"]),
-			"image_url": books[book_id]["image_url"],
-		}
-		authors = []
-		for author in books[book_id]["authors"]["author"]:
-			if type(author) == dict and author.get("name"):
-				authors.append(author["name"])
-		book["authors"] = ", ".join(authors)
-		book["reviews"] = _get_book_reviews(book_id)
-		recommended_books.append(book)
+		if books[book_id]["title"] not in titles:
+			book = {
+				"id": "id" + book_id,
+				"title": books[book_id]["title"],
+				"average_rating": books[book_id]["average_rating"],
+				"image_url": books[book_id]["image_url"],
+			}
+			authors = []
+			for author in books[book_id]["authors"]["author"]:
+				if type(author) == dict and author.get("name"):
+					authors.append(author["name"])
+			book["authors"] = ", ".join(authors)
+			book["reviews"] = _get_book_reviews(book_id)
+			recommended_books.append(book)
+			titles.append(books[book_id]["title"])
 	return recommended_books
 
 
@@ -67,10 +71,10 @@ def _get_recommended_books_detail(recommended_book_ids: list) -> list:
 def search():
 	data = []
 	if request.method == "POST":
-		book_ids = request.form.get("book_ids", "").split()
-		if len(book_ids) >= 1 and book_ids[0] != "":
-			recommended_book_ids = _get_cos_sim_desc(book_ids)
-			data = _get_recommended_books_detail(recommended_book_ids)
+		request_book_ids = request.form.get("book_ids", "").split()
+		if len(request_book_ids) >= 1 and request_book_ids[0] != "":
+			recommended_book_ids = _get_cos_sim_desc(request_book_ids)
+			data = _get_recommended_books_detail(recommended_book_ids, request_book_ids)
 	else:
 		data = []
 	return render_template('search.html', name=project_name, netid=net_id, data=data)
