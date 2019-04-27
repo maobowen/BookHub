ff9d9c0700d736c0119e9aac47a10680e586af9d
Lu <ly298@cornell.edu>
2019-04-26 20:39:57 -0400

    add tagify

--- app/static/js/index_v3.js
+++ app/static/js/index_v3.js
@@ -48,6 +48,35 @@ function validateMyForm(){
     return check();
 }
 
+//tagify part
+var tagInput = document.querySelector('input[name=tags]'),
+    // init Tagify script on the above inputs
+    tagify = new Tagify(tagInput, {
+        whitelist : ["A# .NET", "A# (Axiom)", "A-0 System", "A+", "A++", "ABAP", "ABC", "ABC ALGOL", "ABSET", "ABSYS", "ACC", "Accent", "Ace DASL", "ACL2", "Avicsoft", "ACT-III", "Action!", "ActionScript", "Ada", "Adenine", "Agda", "Agilent VEE", "Agora", "AIMMS", "Alef", "ALF", "ALGOL 58", "ALGOL 60", "ALGOL 68", "ALGOL W", "Alice", "Alma-0", "AmbientTalk", "Amiga E", "AMOS", "AMPL", "Apex (Salesforce.com)", "APL", "AppleScript", "Arc", "ARexx", "Argus", "AspectJ", "Assembly language", "ATS", "Ateji PX", "AutoHotkey", "Autocoder", "AutoIt", "AutoLISP / Visual LISP", "Averest", "AWK", "Axum", "Active Server Pages", "ASP.NET", "B", "Babbage", "Bash", "BASIC", "bc", "BCPL", "BeanShell", "Batch (Windows/Dos)", "Bertrand", "BETA", "Bigwig", "Bistro", "BitC", "BLISS", "Blockly", "BlooP", "Blue", "Boo", "Boomerang", "Bourne shell (including bash and ksh)", "BREW", "BPEL", "B", "C--", "C++ – ISO/IEC 14882", "C# – ISO/IEC 23270", "C/AL", "Caché ObjectScript", "C Shell", "Caml", "Cayenne", "CDuce", "Cecil", "Cesil", "Céu", "Ceylon", "CFEngine", "CFML", "Cg", "Ch", "Chapel", "Charity", "Charm", "Chef", "CHILL", "CHIP-8", "chomski", "ChucK", "CICS", "Cilk", "Citrine (programming language)", "CL (IBM)", "Claire", "Clarion", "Clean", "Clipper", "CLIPS", "CLIST", "Clojure", "CLU", "CMS-2", "COBOL – ISO/IEC 1989", "CobolScript – COBOL Scripting language", "Cobra", "CODE", "CoffeeScript", "ColdFusion", "COMAL", "Combined Programming Language (CPL)", "COMIT", "Common Intermediate Language (CIL)", "Common Lisp (also known as CL)", "COMPASS", "Component Pascal", "Constraint Handling Rules (CHR)", "COMTRAN", "Converge", "Cool", "Coq", "Coral 66", "Corn", "CorVision", "COWSEL", "CPL", "CPL", "Cryptol", "csh", "Csound", "CSP", "CUDA", "Curl", "Curry", "Cybil", "Cyclone", "Cython", "M2001", "M4", "M#", "Machine code", "MAD (Michigan Algorithm Decoder)", "MAD/I", "Magik", "Magma", "make", "Maple", "MAPPER now part of BIS", "MARK-IV now VISION:BUILDER", "Mary", "MASM Microsoft Assembly x86", "MATH-MATIC", "Mathematica", "MATLAB", "Maxima (see also Macsyma)", "Max (Max Msp – Graphical Programming Environment)", "Maya (MEL)", "MDL", "Mercury", "Mesa", "Metafont", "Microcode", "MicroScript", "MIIS", "Milk (programming language)", "MIMIC", "Mirah", "Miranda", "MIVA Script", "ML", "Model 204", "Modelica", "Modula", "Modula-2", "Modula-3", "Mohol", "MOO", "Mortran", "Mouse", "MPD", "Mathcad", "MSIL – deprecated name for CIL", "MSL", "MUMPS", "Mystic Programming L"],
+        blacklist : [".NET", "PHP"], // <-- passed as an attribute in this demo
+    });
+
+// "remove all tags" button event listener
+document.querySelector('.tags--removeAllBtn')
+    .addEventListener('click', tagify.removeAllTags.bind(tagify))
+
+
+//function to clean tag input
+function clean_text(text){
+    //.replace()
+    return text.trim()
+}
+
+//function to get tags
+function getCleanTags(){
+    var raw_tag_list = tagify.value, 
+    tags = "";
+    raw_tag_list.forEach(function(tag_json) {
+        tags = tags + clean_text(tag_json['value']) + " "
+    }); 
+    return tags
+}
+
 //check function
 function check(){
     var book_t1 = document.getElementById('book_title_1').innerText,
@@ -58,8 +87,9 @@ function check(){
         check_valid = true,
         id_list = "",
         titles = [book_t1, book_t2, book_t3, book_t4, book_t5], 
-        id_title_dict = {};
-
+        id_title_dict = {}, 
+        tags_inputed = getCleanTags();
+    
     for (var i = 0; i < 5; i++){
         var id = "#card_" + (1 + i);
         if ((titles[i] !== "") && !(book_titles.includes(titles[i]))){
@@ -78,6 +108,7 @@ function check(){
         //alert(id_list);
         //$("#").value = id_list;
         document.getElementById('book_ids').value = id_list;
+        document.getElementById('tags').value = tags_inputed;
         id_title_dict['1'] = book_t1;
         id_title_dict['2'] = book_t2;
         id_title_dict['3'] = book_t3;
@@ -216,3 +247,48 @@ $(document).ready(function() {
         });
     });
 });
+
+/*
+// Chainable event listeners
+tagify.on('add', onAddTag)
+      .on('remove', onRemoveTag)
+      .on('input', onInput)
+      .on('edit', onTagEdit)
+      .on('invalid', onInvalidTag)
+      .on('click', onTagClick);
+
+// tag added callback
+function onAddTag(e){
+    console.log("onAddTag: ", e.detail);
+    console.log("original input value: ", input.value)
+    tagify.off('add', onAddTag) // exmaple of removing a custom Tagify event
+}
+
+// tag remvoed callback
+function onRemoveTag(e){
+    console.log(e.detail);
+    console.log("tagify instance value:", tagify.value)
+}
+
+// on character(s) added/removed (user is typing/deleting)
+function onInput(e){
+    console.log(e.detail);
+    console.log("onInput: ", e.detail);
+}
+
+function onTagEdit(e){
+    console.log("onTagEdit: ", e.detail);
+}
+
+// invalid tag added callback
+function onInvalidTag(e){
+    console.log("onInvalidTag: ", e.detail);
+}
+
+// invalid tag added callback
+function onTagClick(e){
+    console.log(e.detail);
+    console.log("onTagClick: ", e.detail);
+}
+
+*/
